ddfd0d6ddfaba9f3adc903c340b8d8fee8b72c51
Irfan Ullah <iukhan53@gmail.com>
2023-08-19 22:39:47 +0500

    mobile update

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import WebView from "react-native-webview";
+import Constants from 'expo-constants';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <>
+            <StatusBar style="auto"/>
+            <View style={{ flex: 1, marginTop: Constants.statusBarHeight }}>
+                <WebView
+                    style={styles.container}
+                    source={{uri: 'https://monitoring-backend.tameerekhalaq.org/login'}}
+                />
+            </View>
+
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
 });
